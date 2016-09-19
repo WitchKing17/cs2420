a95ce46c0270fe48be27747d03e4517727491bb2
WitchKing17 <tlaffy33@yahoo.com>
2016-09-19 11:20:50 -0600

    Created Stack.h and initialized the code for the Stack class

---
+++ Stack/Stack/Stack.h
@@ -0,0 +1,21 @@
+//Spencer Cheshire
+//CS 2420-601
+//September 19th, 2016
+//Assignment 3 - Stack
+//I, Spencer Cheshire, have not used any code other than my own (or that in the textbook) for this project. I
+//also have not used any function or data-structure from the Standard-Template Library. I
+//understand that any violation of this disclaimer will result in a 0 for the project
+
+#include <string>
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+class Stack
+{
+public:
+
+private:
+
+};
