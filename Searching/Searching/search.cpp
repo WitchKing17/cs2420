570b0d1febb46826d4c032861f945b140bfd5f48
WitchKing17 <tlaffy33@yahoo.com>
2016-09-26 19:14:34 -0600

    Added the Google Test files into the project and initiated the RandomArray class in search.h and search.cpp

--- Searching/Searching/search.cpp
+++ Searching/Searching/search.cpp
@@ -1,14 +1,13 @@
 //Spencer Cheshire
-//CS 2420 - Assignment 4
+//CS 2420 - Assignment 4 source code
 //September 26th, 2016
 //I, Spencer Cheshire, have not used any code other than my own (or that in the textbook) for this project. I
 //also have not used any function or data-structure from the Standard-Template Library. I
 //understand that any violation of this disclaimer will result in a 0 for the project
 
-class RandomArray
+#include "search.h"
+
+RandomArray::RandomArray(size_t _cap) 
 {
-public:
-	RandomArray(size_t);
-private:
-	size_t capacity;
-};
+		capacity = _cap;
+}
